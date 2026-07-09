705e9e463a39eb61c02cd618b2dc372c1608ea24
Pallavi-0202 <pallavichandra933@gmail.com>
2026-07-09 17:34:19 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+
+// ============================
+// History Questions
+// ============================
+
+const questions = [
+  {
+    id: 1,
+    question: "Who founded the Maurya Empire?",
+    options: [
+      "Ashoka",
+      "Chandragupta Maurya",
+      "Bindusara",
+      "Harsha"
+    ],
+    answer: "Chandragupta Maurya"
+  },
+  {
+    id: 2,
+    question: "In which year did India gain independence?",
+    options: [
+      "1945",
+      "1946",
+      "1947",
+      "1950"
+    ],
+    answer: "1947"
+  },
+  {
+    id: 3,
+    question: "Who wrote the Arthashastra?",
+    options: [
+      "Kalidasa",
+      "Chanakya",
+      "Aryabhata",
+      "Tulsidas"
+    ],
+    answer: "Chanakya"
+  }
+];
+
+// ============================
+// Home Route
+// ============================
+
+app.get("/", (req, res) => {
+  res.send("History Quiz Backend is Running!");
+});
+
+// ============================
+// Questions API
+// ============================
+
+app.get("/api/questions", (req, res) => {
+  res.json(questions);
+});
+
+// ============================
+// Start Server
+// ============================
+
+app.listen(5000, () => {
+  console.log("Server is running on http://localhost:5000");
+});
+
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,149 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import history from "./assets/history.jpg";
+
+
+function App() {
+
+
+
+
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+const [score, setScore] = useState(0);
+const [selectedAnswer, setSelectedAnswer] = useState("");
+const [quizFinished, setQuizFinished] = useState(false);
+const [startQuiz, setStartQuiz] = useState(false);
+
+
+  useEffect(() => {
+
+    axios
+      .get("http://localhost:5000/api/questions")
+      .then((response) => {
+        setQuestions(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  }, []);
+const current = questions[currentQuestion];
+const handleNext = () => {
+
+  if (selectedAnswer === current.answer) {
+    setScore(score + 1);
+  }
+
+  setSelectedAnswer("");
+
+  if (currentQuestion < questions.length - 1) {
+    setCurrentQuestion(currentQuestion + 1);
+} else {
+    setQuizFinished(true);
+}
+
+
+};
+if (!startQuiz) {
+  return (
+  <div
+  className="app"
+  style={{
+    backgroundImage: `linear-gradient(rgba(5,10,25,0.82), rgba(8,17,32,0.82)), url(${history})`,
+  }}
+>
+<h1>🧭 BHARAT QUEST</h1>
+
+
+      <p className="tagline">
+        Journey Through India's Glorious Past
+      </p>
+     <p className="description">
+Test your knowledge of Ancient, Medieval and Modern Indian History through an engaging interactive quiz.
+</p>
+
+
+<button
+        className="start-btn"
+        onClick={() => setStartQuiz(true)}
+      >
+        Start Quiz →
+      </button>
+
+    </div>
+  );
+
+}
+if (quizFinished) {
+  return (
+    <div className = "app"
+
+style={{
+  backgroundImage: `linear-gradient(rgba(5,10,25,0.82), rgba(8,17,32,0.82)), url(${history})`,
+}}>
+
+
+      <h1>🏆 Quiz Completed!</h1>
+
+      <h2 className="score">
+  Your Score: {score} / {questions.length}
+</h2>
+
+
+      <button onClick={() => window.location.reload()}>
+        Play Again
+      </button>
+    </div>
+  );
+  
+}
+return(
+    <div className = "app"
+      style={{
+    backgroundImage: `linear-gradient(rgba(5,10,25,0.82), rgba(8,17,32,0.82)), url(${history})`,
+  }}>
+    
+<h1>🧭 BHARAT QUEST</h1>
+
+  {current && (
+  <div className ="quiz-card">
+
+    <h2>Question {currentQuestion + 1}</h2>
+
+    <h3>{current.question}</h3>
+
+    <ul>
+      {current.options.map((option, index) => (
+        <li key={index}>
+          <button
+  className={selectedAnswer === option ? "selected" : ""}
+  onClick={() => setSelectedAnswer(option)}
+>
+  {option}
+</button>
+
+        </li>
+      ))}
+    </ul>
+<p>
+  Selected Answer: {selectedAnswer || "None"}
+</p>
+<button onClick={handleNext}
+disabled = {selectedAnswer===""}>
+  Next
+</button>
+
+
+
+  </div>
+)}
+
+    </div>
+  );
+}
+
+export default App;
+
+
